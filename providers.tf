2635e73b3943819283117ebb375b565fb81fca34
EricHsieh0806 <30400643+EricHsieh0806@users.noreply.github.com>
2022-12-13 10:08:15 +0800

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -27,10 +27,6 @@ terraform {
 provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   region       = var.region
-
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
 }
 
 provider "oci" {
